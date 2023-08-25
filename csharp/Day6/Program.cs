76ba8df2aa8ad4b622ee21f691fc2d0dffdb6f14
Matt Wilding <matt.wilding@rwwa.com.au>
2023-08-25 15:23:25 +0800

    Day 6

---
+++ csharp/Day6/Program.cs
@@ -0,0 +1,24 @@
+﻿var input = await File.ReadAllLinesAsync("input.txt");
+var buffer = input.First();
+
+void ProcessMoves(string part, uint consecutive)
+{
+    var queue = new Queue<char>();
+    for (int i = 0; i < buffer.Length; i++)
+    {
+        if (queue.Count >= consecutive)
+        {
+            queue.Dequeue();
+        }
+        queue.Enqueue(buffer[i]);
+
+        if (queue.Distinct().Count() == consecutive)
+        {
+            Console.WriteLine($"Part {part}: {i + 1}");
+            break;
+        }
+    }
+}
+
+ProcessMoves("One", 4);
+ProcessMoves("Two", 14);
