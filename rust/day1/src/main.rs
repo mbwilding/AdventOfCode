2aee6e56ef27f8047c82aef01ebe874fc10de6f3
Matt Wilding <matt.wilding@rwwa.com.au>
2023-08-26 15:50:27 +0800

    Rust update

--- rust/day1/src/main.rs
+++ rust/day1/src/main.rs
@@ -1,4 +1,12 @@
 fn main() {
-    let lines = common::read_lines("../../../!data/day1/real.txt").unwrap();
-    println!("Hello, world!");
+    let reader = common::read_lines("../!data/day1/real.txt").expect("Invalid path");
+
+    for line in reader {
+        match line {
+            Ok(l) => {
+                println!("{l}")
+            },
+            Err(e) => eprintln!("Error reading line: {e}"),
+        }
+    }
 }
