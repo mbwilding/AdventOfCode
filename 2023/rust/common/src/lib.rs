a43c251ffc9f315bb29efd5af446eced54c78285
Matthew Wilding <mbwilding@gmail.com>
2023-12-01 13:56:17 +0800

    Passed level 1

--- 2023/rust/common/src/lib.rs
+++ 2023/rust/common/src/lib.rs
@@ -2,12 +2,13 @@ use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::path::Path;
 
-pub fn read_lines<P>(path: P) -> io::Result<impl Iterator<Item = String>>
-where
-    P: AsRef<Path>,
+pub fn read_lines<P>(path: P) -> io::Result<Vec<String>>
+    where
+        P: AsRef<Path>,
 {
     let file = File::open(path)?;
     let reader = BufReader::new(file);
 
-    Ok(reader.lines().map_while(Result::ok))
+    let lines = reader.lines().collect::<Result<Vec<_>, io::Error>>()?;
+    Ok(lines)
 }
--- 2023/rust/day1/src/main.rs
+++ 2023/rust/day1/src/main.rs
@@ -1,13 +1,104 @@
+use std::collections::HashMap;
 use common_2023::read_lines;
 
 fn main() {
-    let _lines = read_lines("2023/!data/day1/real.txt").expect("Failed to read lines from file");
+    let lines = read_lines("2023/!data/day1/real.txt").expect("Failed to read lines from file");
 
-    // let sorted_calories = process_lines(lines);
-    // println!("Part One: {}", sorted_calories[0]);
+    let numbers = process_lines_part_1(&lines);
+    let mut sum = 0;
+    for line in numbers {
+        let first = line.first().unwrap_or(&0);
+        let last = line.last().unwrap_or(&1);
+        let concatenated_str = first.to_string() + &last.to_string();
+        let result: u32 = concatenated_str.parse().unwrap();
+        sum += result;
+    }
+    println!("Part One: {:?}", sum);
 
-    // let top_3_sum: usize = sorted_calories.iter().take(3).sum();
-    // println!("Part Two: {}", top_3_sum);
+    let part_2 = process_lines_part_2(&lines);
+    println!("Part Two: {:?}", part_2);
+}
 
-    println!("Hello World!");
+fn process_lines_part_1(lines: &Vec<String>) -> Vec<Vec<u32>> {
+    let mut container_numbers: Vec<Vec<u32>> = Vec::new();
+
+    for line in lines {
+        if line.is_empty() {
+
+        } else {
+            let mut characters: Vec<char> = Vec::new();
+            let mut numbers: Vec<u32> = Vec::new();
+
+            for character in line.chars() {
+                characters.push(character);
+                if let Some(value) = character.to_digit(10) {
+                    numbers.push(value)
+                }
+            }
+
+            container_numbers.push(numbers)
+        }
+    }
+
+    return container_numbers;
+}
+
+fn process_lines_part_2(lines: &Vec<String>) -> i32 {
+    let mut sum = 0;
+
+    for line in lines {
+        let value = find_first_last_digit(line);
+        sum += value;
+    }
+
+    sum
+}
+
+fn find_first_last_digit(line: &str) -> i32 {
+    let number_map = create_number_map();
+    let mut first_digit = 0;
+    let mut last_digit = 0;
+    let mut current_word = String::new();
+
+    for c in line.chars() {
+        if c.is_alphabetic() {
+            current_word.push(c);
+            for i in 0..current_word.len() {
+                if let Some(&number) = number_map.get(current_word[i..].to_lowercase().as_str()) {
+                    if first_digit == 0 {
+                        first_digit = number;
+                    }
+                    last_digit = number;
+                    break;
+                }
+            }
+        } else if c.is_digit(10) {
+            let digit = c.to_digit(10).unwrap() as i32;
+            if first_digit == 0 {
+                first_digit = digit;
+            }
+            last_digit = digit;
+            current_word.clear();
+        } else {
+            current_word.clear();
+        }
+    }
+
+    let concatenated_str = first_digit.to_string() + &last_digit.to_string();
+    concatenated_str.parse().unwrap()
+}
+
+fn create_number_map() -> HashMap<String, i32> {
+    let mut map = HashMap::new();
+    map.insert("one".to_string(), 1);
+    map.insert("two".to_string(), 2);
+    map.insert("three".to_string(), 3);
+    map.insert("four".to_string(), 4);
+    map.insert("five".to_string(), 5);
+    map.insert("six".to_string(), 6);
+    map.insert("seven".to_string(), 7);
+    map.insert("eight".to_string(), 8);
+    map.insert("nine".to_string(), 9);
+
+    map
 }
