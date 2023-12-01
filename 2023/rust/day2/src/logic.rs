1e507c76073ad66bed62df8432ce5d6591dbae4b
Matthew Wilding <mbwilding@gmail.com>
2023-12-01 22:13:07 +0800

    Prep for day2 added

---
+++ 2023/rust/day2/src/logic.rs
@@ -0,0 +1,7 @@
+pub fn part_1(_lines: &[String]) -> u32 {
+    0
+}
+
+pub fn part_2(_lines: &[String]) -> u32 {
+    0
+}
--- 2023/rust/day2/src/tests.rs
+++ 2023/rust/day2/src/tests.rs
@@ -6,21 +6,21 @@ mod tests {
 
     #[test]
     fn mock_part_1_test() {
-        test(DAY, "mock1", part_1, 142);
+        test(DAY, "mock1", part_1, 0);
     }
 
     #[test]
     fn mock_part_2_test() {
-        test(DAY, "mock2", part_2, 281);
+        test(DAY, "mock2", part_2, 0);
     }
 
     #[test]
     fn real_part_1_test() {
-        test(DAY, "real", part_1, 54573);
+        test(DAY, "real", part_1, 0);
     }
 
     #[test]
     fn real_part_2_test() {
-        test(DAY, "real", part_2, 54591);
+        test(DAY, "real", part_2, 0);
     }
 }
