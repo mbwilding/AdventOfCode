90e0f25939df176f1bbd08dbe0d2bb395597a49d
Matthew Wilding <mbwilding@gmail.com>
2023-12-02 13:35:43 +0800

    Refactor

--- 2023/rust/day2/src/logic.rs
+++ 2023/rust/day2/src/logic.rs
@@ -1,29 +1,26 @@
 use rayon::prelude::*;
 use std::collections::HashMap;
 
-pub fn part_1(lines: &[String]) -> i32 {
+pub fn part_1(lines: &[String]) -> u32 {
     let max_cubes = HashMap::from([("red", 12), ("green", 13), ("blue", 14)]);
 
     lines
         .par_iter()
         .filter_map(|line| {
             let parts: Vec<&str> = line.split(": ").collect();
-            let game_id: i32 = parts[0].split_whitespace().last().unwrap().parse().unwrap();
-            let mut sets = parts[1].split("; ");
+            let game_id: u32 = parts[0].split_whitespace().last().unwrap().parse().unwrap();
 
-            if sets.all(|set| {
-                set.split(", ")
-                    .map(|c| c.split_whitespace().collect::<Vec<&str>>())
-                    .all(|vec| {
-                        let count: i32 = vec[0].parse().unwrap();
+            parts[1]
+                .split("; ")
+                .all(|set| {
+                    set.split(", ").all(|c| {
+                        let vec: Vec<&str> = c.split_whitespace().collect();
+                        let count: u32 = vec[0].parse().unwrap();
                         let color = vec[1];
                         count <= *max_cubes.get(color).unwrap()
                     })
-            }) {
-                Some(game_id)
-            } else {
-                None
-            }
+                })
+                .then_some(game_id)
         })
         .sum()
 }
@@ -32,22 +29,24 @@ pub fn part_2(lines: &[String]) -> u32 {
     lines
         .par_iter()
         .map(|game| {
-            let sets = game.split(": ").nth(1).unwrap().split("; ");
-            let mut min_cubes: HashMap<&str, u32> = HashMap::new();
-
-            for set in sets {
-                set.split(", ").for_each(|cube| {
-                    let parts: Vec<&str> = cube.split_whitespace().collect();
-                    let count = parts[0].parse::<u32>().unwrap();
-                    let color = parts[1];
+            game.split(": ")
+                .nth(1)
+                .unwrap()
+                .split("; ")
+                .fold(HashMap::new(), |mut min_cubes, set| {
+                    set.split(", ").for_each(|cube| {
+                        let parts: Vec<&str> = cube.split_whitespace().collect();
+                        let count = parts[0].parse::<u32>().unwrap();
+                        let color = parts[1];
+                        min_cubes
+                            .entry(color)
+                            .and_modify(|e| *e = u32::max(*e, count))
+                            .or_insert(count);
+                    });
                     min_cubes
-                        .entry(color)
-                        .and_modify(|e| *e = u32::max(*e, count))
-                        .or_insert(count);
-                });
-            }
-
-            min_cubes.values().product::<u32>()
+                })
+                .values()
+                .product::<u32>()
         })
         .sum()
 }
