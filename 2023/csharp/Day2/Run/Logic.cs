173241cfd92edf3acf55355e9c57ef900363a350
Matthew Wilding <mbwilding@gmail.com>
2023-12-02 15:00:53 +0800

    More fluent

--- 2023/csharp/Day2/Run/Logic.cs
+++ 2023/csharp/Day2/Run/Logic.cs
@@ -28,21 +28,16 @@ public static class Logic
     public static int Part2(IEnumerable<string> lines) =>
         lines
             .AsParallel()
-            .Select(line =>
-            {
-                var cubes = line.Split(": ")[1]
-                    .Split("; ")
-                    .SelectMany(set => set.Split(", "))
-                    .Select(cube =>
-                    {
-                        var parts = cube.Split();
-                        return (Color: parts[1], Count: int.Parse(parts[0]));
-                    })
-                    .GroupBy(c => c.Color)
-                    .Select(g => g.Max(c => c.Count))
-                    .Aggregate(1, (product, count) => product * count);
-
-                return cubes;
-            })
+            .Select(line => line.Split(": ")[1]
+                .Split("; ")
+                .SelectMany(set => set.Split(", "))
+                .Select(cube =>
+                {
+                    var parts = cube.Split();
+                    return (Color: parts[1], Count: int.Parse(parts[0]));
+                })
+                .GroupBy(c => c.Color)
+                .Select(g => g.Max(c => c.Count))
+                .Aggregate(1, (product, count) => product * count))
             .Sum();
 }
