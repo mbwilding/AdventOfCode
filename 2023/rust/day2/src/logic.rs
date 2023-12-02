30485b5075fc31823f627d082ca9d204ff42b762
Matthew Wilding <mbwilding@gmail.com>
2023-12-02 13:24:45 +0800

    Rayon para iter for performance

--- 2023/rust/day2/src/logic.rs
+++ 2023/rust/day2/src/logic.rs
@@ -1,13 +1,11 @@
+use rayon::prelude::*;
 use std::collections::HashMap;
 
 pub fn part_1(lines: &[String]) -> i32 {
-    let max_cubes = HashMap::from([
-        ("red", 12),
-        ("green", 13),
-        ("blue", 14),
-    ]);
+    let max_cubes = HashMap::from([("red", 12), ("green", 13), ("blue", 14)]);
 
-    lines.iter()
+    lines
+        .par_iter()
         .filter_map(|line| {
             let parts: Vec<&str> = line.split(": ").collect();
             let game_id: i32 = parts[0].split_whitespace().last().unwrap().parse().unwrap();
@@ -31,21 +29,22 @@ pub fn part_1(lines: &[String]) -> i32 {
 }
 
 pub fn part_2(lines: &[String]) -> i32 {
-    lines.iter()
+    lines
+        .par_iter()
         .map(|game| {
             let sets = game.split(": ").nth(1).unwrap().split("; ");
             let mut min_cubes: HashMap<&str, i32> = HashMap::new();
 
             for set in sets {
-                set.split(", ")
-                    .for_each(|cube| {
-                        let parts: Vec<&str> = cube.split_whitespace().collect();
-                        let count = parts[0].parse::<i32>().unwrap();
-                        let color = parts[1];
-                        min_cubes.entry(color)
-                            .and_modify(|e| *e = i32::max(*e, count))
-                            .or_insert(count);
-                    });
+                set.split(", ").for_each(|cube| {
+                    let parts: Vec<&str> = cube.split_whitespace().collect();
+                    let count = parts[0].parse::<i32>().unwrap();
+                    let color = parts[1];
+                    min_cubes
+                        .entry(color)
+                        .and_modify(|e| *e = i32::max(*e, count))
+                        .or_insert(count);
+                });
             }
 
             min_cubes.values().product::<i32>()
