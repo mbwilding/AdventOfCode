3e192ec5cc5b6e7ed95372c89a36d220089bdd70
Matthew Wilding <mbwilding@gmail.com>
2023-12-06 14:04:38 +0800

    Updated day 6 tests

--- 2023/rust/day6/src/tests.rs
+++ 2023/rust/day6/src/tests.rs
@@ -11,16 +11,16 @@ mod tests {
 
     #[test]
     fn part_1_real() {
-        test(DAY, "real", part_1, 288);
+        test(DAY, "real", part_1, 393120);
     }
 
     #[test]
     fn part_2_mock() {
-        test(DAY, "mock", part_2, 0);
+        test(DAY, "mock", part_2, 71503);
     }
 
     #[test]
     fn part_2_real() {
-        test(DAY, "real", part_2, 0);
+        test(DAY, "real", part_2, 36872656);
     }
 }
